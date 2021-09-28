c45c994bcfd039f3ea4e12ad81b943dc30bc4db9
Michal3456 <troianorum@wp.pl>
2021-09-28 15:57:53 +0200

    Add file zad1.php in cw1

---
+++ cw1/zad1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<style>
+border: 1px solid black;
+</style>
+<body>
+
+
+<table>
+<tbody>
+<tr>
+  <?php
+  $krotka = array("Naglowek1"=>"Kolumna1", "Naglowek2"=>"Kolumna2", "Naglowek3"=>"Kolumna3");
+
+  foreach($krotka as $key => $val) {
+    echo "<td>$key</td>";
+  }
+  ?>
+</tr>
+<tr>
+  <?php
+  $krotka = array("Naglowek1"=>"Kolumna1", "Naglowek2"=>"Kolumna2", "Naglowek3"=>"Kolumna3");
+
+  foreach($krotka as $key => $val) {
+    echo "<td>$val</td>";
+  }
+  ?>
+</tr>
+</tbody>
+</table>
+
+</body>
+</html>
